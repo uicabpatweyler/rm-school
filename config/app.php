585a40ba935d7a7ade4cc449d862e17edcddbf3f
uicabpatweyler <uicabpatweyler@gmail.com>
2022-11-30 16:05:02 -0500

    Add Spatie: Laravel Permission
    Install and set the initial configuration for Spatie: Laravel Permission

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Application Service Providers...
